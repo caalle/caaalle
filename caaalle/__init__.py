c4d4f96475495ba113575860f8e42410a13c8de3
Carl Claesson <cfclaesson@gmail.com>
2020-03-17 20:55:00 +0100

    feat: add semantic release

--- caaalle/__init__.py
+++ caaalle/__init__.py
@@ -5,6 +5,8 @@ caaalle
 The caaalle package.
 """
 
+__version__ = "1.0.0"
+
 def add(initial: int=0, number: int=0) -> int:
     """Return sum of *intial* and *number*.
     :param initial: Initial value.
--- setup.py
+++ setup.py
@@ -3,32 +3,44 @@
 import os
 from setuptools import setup
 
-# get key package details from caaalle/__version__.py
-about = {}  # type: ignore
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'caaalle', '__version__.py')) as f:
-    exec(f.read(), about)
-
-# load the README file and use it as the long_description for PyPI
 with open('README.md', 'r') as f:
     readme = f.read()
 
-# package configuration - for reference see:
-# https://setuptools.readthedocs.io/en/latest/setuptools.html#id9
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+_title = 'caaalle'
+_description = 'caaalle'
+_author = 'Carl Larsson'
+_author_email = 'example@gmail.com'
+_license = 'Apache 2.0'
+_url = 'https://github.com/caalle/caaalle'
+
 setup(
-    name=about['__title__'],
-    description=about['__description__'],
+    name=_title,
+    description=_description,
     long_description=readme,
     long_description_content_type='text/markdown',
-    version=about['__version__'],
-    author=about['__author__'],
-    author_email=about['__author_email__'],
-    url=about['__url__'],
+    version=find_version("package", "__init__.py"),
+    author=_author,
+    author_email=_author_email,
+    url=_url,
     packages=['caaalle'],
     include_package_data=True,
     python_requires=">=3.8.*",
     install_requires=[],
-    license=about['__license__'],
+    license=_license,
     zip_safe=False,
     entry_points={
         'caaalle': ['caaalle=caaalle.entry_points:main'],
