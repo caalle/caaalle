d1fcb90b7b6b1fcf98e0d1aea8587332bdaa1849
Carl Claesson <cfclaesson@gmail.com>
2020-03-17 21:43:15 +0100

    fix: docs not building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
-from caaalle.__version__ import __version__
+from caaalle import __version__
 
 
 # -- Project information -----------------------------------------------------
