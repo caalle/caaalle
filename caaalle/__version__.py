9120b30b15f16f6edde81af71f8936515d58d152
Carl Claesson <cfclaesson@gmail.com>
2020-03-17 20:22:34 +0100

    Bump version

--- caaalle/__version__.py
+++ caaalle/__version__.py
@@ -7,7 +7,7 @@ Information about the current version of the caaalle package.
 
 __title__ = 'caaalle'
 __description__ = 'caaalle'
-__version__ = '0.0.0'
+__version__ = '1.0.0'
 __author__ = 'Carl Larsson'
 __author_email__ = 'n/a'
 __license__ = 'Apache 2.0'
